1c84b7cac931b719e77bc5c09dd700eb21b9c075
CHIRAG <cgupta_be21@thapar.edu>
2023-07-25 20:25:30 +0530

    Model routes controller created

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("Hello world");
+const express = require('express');
+const server = express();
+const connectToMongo = require('./db');
+connectToMongo();
+const ProductsRouter = require("./routes/Product");
+const categoryRouter = require("./routes/Category");
+const brandsRouter = require("./routes/Brands");
+const userRouter = require("./routes/User");
+const cors = require('cors');
+
+const port = process.env.PORT || 8080
+
+//Middlewares
+server.use(cors({
+    exposedHeaders:['X-Total-Count']
+}))
+server.use(express.json()); //to parse req.body
+server.use('/products',ProductsRouter.router)
+server.use('/category',categoryRouter.router);
+server.use('/brands',brandsRouter.router);
+server.use('/user',userRouter.router);
+
+
+
+
+
+server.get('/',(req,res)=>{
+    res.json({status:'success'});
+})
+
+
+
+server.listen(port,()=>{
+    console.log(`listnening at http://localhost:${port}`);
+})
---
+++ model/Product.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose
+
+const ProductSchema = new Schema({
+    title: {type:String, required: true, unique: true},
+    description: {type:String, required: true},
+    price: {type:Number, required: true, min:[0,'wrong min price'], max:[1000000,'worng max price']},
+    discountPercentage: {type:Number, required: true, min:[0,'wrong min discount'], max:[100,'worng max discount']},
+    rating: {type:Number, required: true, min:[0,'wrong min rating'], max:[5,'worng max rating']},
+    stock: {type:Number, min:[0,'wrong min stock'],default:0},
+    brand: {type:String,required: true},
+    category :{type:String, required:true},
+    thumbnail: {type: String, required: true},
+    images: {type: [String],required:true},
+    admin: {type:String, required:true},
+    deleted: {type:Boolean, default:false},
+})
+
+const virtual = ProductSchema.virtual('id');
+virtual.get(function(){
+    return this._id;
+})
+
+ProductSchema.set('toJSON',{
+    virtuals: true,
+    versionKey: false,
+    transform: function(doc,ret){delete ret._id}
+})
+
+exports.Product = mongoose.model('Product',ProductSchema)
