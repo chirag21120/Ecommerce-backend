423b0b49a0a20d19bb189cd27ed6dde27b1d5fbf
CHIRAG <cgupta_be21@thapar.edu>
2023-08-22 19:43:19 +0530

    spelling error

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ server.post('/webhook', express.raw({type: 'application/json'}),async (request, 
       case 'payment_intent.succeeded':
         const paymentIntentSucceeded = event.data.object;
         const order = await Order.findById(paymentIntentSucceeded.metadata.orderId);
-        order.paymentStatus = 'recieved';
+        order.paymentStatus = 'received';
         await order.save();
         break;
       // ... handle other event types
